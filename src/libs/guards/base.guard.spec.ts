f1a13a72c89969608364668e56e66b0cf3b7d9ed
pedro <alvespedro991@gmail.com>
2023-07-08 23:43:39 +0100

    small fix

--- src/libs/guards/base.guard.spec.ts
+++ src/libs/guards/base.guard.spec.ts
@@ -4,66 +4,77 @@ import { RoleType } from './role-type.enum';
 import { Reflector } from '@nestjs/core';
 
 class TestHelperGuard extends BaseGuard {
-    constructor(reflector: Reflector) {
-        super(reflector);
-    }
+  constructor(reflector: Reflector) {
+    super(reflector);
+  }
 
-    canActivate(context: ExecutionContext): Promise<boolean> | boolean {
-        return true;
-    }
+  canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+    return true;
+  }
 }
 
 describe('BaseGuard', () => {
-    let guard: BaseGuard;
-    let reflectorMock: jest.Mocked<Reflector>;
+  let guard: BaseGuard;
+  let reflectorMock: jest.Mocked<Reflector>;
 
-    beforeEach(() => {
-        reflectorMock = {
-            get: jest.fn(),
-        } as unknown as jest.Mocked<Reflector>;
+  beforeEach(() => {
+    reflectorMock = {
+      get: jest.fn(),
+    } as unknown as jest.Mocked<Reflector>;
 
-        guard = new TestHelperGuard(reflectorMock);
+    guard = new TestHelperGuard(reflectorMock);
+  });
+
+  describe('handleRoute', () => {
+    it('should throw UnauthorizedException when roles are not defined', () => {
+      const contextMock = {
+        getHandler: jest.fn(),
+      } as unknown as ExecutionContext;
+      const roleType = RoleType.MANAGER;
+
+      reflectorMock.get.mockReturnValueOnce(undefined);
+
+      expect(() => guard.handleRoute(contextMock, roleType)).toThrow(
+        UnauthorizedException,
+      );
+      expect(reflectorMock.get).toHaveBeenCalledWith(
+        'role',
+        contextMock.getHandler(),
+      );
     });
 
-    describe('handleRoute', () => {
-        it('should throw UnauthorizedException when roles are not defined', () => {
-            const contextMock = {
-                getHandler: jest.fn(),
-            } as unknown as ExecutionContext;
-            const roleType = RoleType.MANAGER;
+    it('should return true when the provided roleType is included in the roles array', () => {
+      const contextMock = {
+        getHandler: jest.fn(),
+      } as unknown as ExecutionContext;
+      const roleType = RoleType.MANAGER;
 
-            reflectorMock.get.mockReturnValueOnce(undefined);
+      reflectorMock.get.mockReturnValueOnce(['manager', 'technician']);
 
-            expect(() => guard.handleRoute(contextMock, roleType)).toThrow(UnauthorizedException);
-            expect(reflectorMock.get).toHaveBeenCalledWith('role', contextMock.getHandler());
-        });
+      const result = guard.handleRoute(contextMock, roleType);
 
-        it('should return true when the provided roleType is included in the roles array', () => {
-            const contextMock = {
-                getHandler: jest.fn(),
-            } as unknown as ExecutionContext;
-            const roleType = RoleType.MANAGER;
-
-            reflectorMock.get.mockReturnValueOnce(['manager', 'technician']);
-
-            const result = guard.handleRoute(contextMock, roleType);
-
-            expect(result).toBe(true);
-            expect(reflectorMock.get).toHaveBeenCalledWith('role', contextMock.getHandler());
-        });
-
-        it('should return false when the provided roleType is not included in the roles array', () => {
-            const contextMock = {
-                getHandler: jest.fn(),
-            } as unknown as ExecutionContext;
-            const roleType = RoleType.TECHNICIAN;
-
-            reflectorMock.get.mockReturnValueOnce(['manager', 'admin']);
-
-            const result = guard.handleRoute(contextMock, roleType);
-
-            expect(result).toBe(false);
-            expect(reflectorMock.get).toHaveBeenCalledWith('role', contextMock.getHandler());
-        });
+      expect(result).toBe(true);
+      expect(reflectorMock.get).toHaveBeenCalledWith(
+        'role',
+        contextMock.getHandler(),
+      );
     });
+
+    it('should return false when the provided roleType is not included in the roles array', () => {
+      const contextMock = {
+        getHandler: jest.fn(),
+      } as unknown as ExecutionContext;
+      const roleType = RoleType.TECHNICIAN;
+
+      reflectorMock.get.mockReturnValueOnce(['manager', 'admin']);
+
+      const result = guard.handleRoute(contextMock, roleType);
+
+      expect(result).toBe(false);
+      expect(reflectorMock.get).toHaveBeenCalledWith(
+        'role',
+        contextMock.getHandler(),
+      );
+    });
+  });
 });
